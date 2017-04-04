7d3134b3c0c5bf67d8cd1a1acabd1759e692d1cf
hzhz2018 <shayu2017@163.com>
2017-04-05 07:20:17 +0800

    good

--- app/src/main/java/com/app/yangyang/zhbj/fragment/ContentFragment.java
+++ app/src/main/java/com/app/yangyang/zhbj/fragment/ContentFragment.java
@@ -1,6 +1,7 @@
 package com.app.yangyang.zhbj.fragment;
 
 import android.view.View;
+import android.widget.RadioGroup;
 
 import com.app.yangyang.zhbj.R;
 
@@ -11,9 +12,18 @@ import com.app.yangyang.zhbj.R;
 public class ContentFragment extends BaseFragment {
 
 
+    private RadioGroup rg_group;
+
     @Override
     public View initView() {
         View  view = View.inflate(mActivity, R.layout.fragment_content_menu,null);
+
+        rg_group = (RadioGroup) view.findViewById(R.id.rg_group);
         return view;
+    }
+
+    @Override
+    public void initData() {
+        rg_group.check(R.id.rb_home);
     }
 }
